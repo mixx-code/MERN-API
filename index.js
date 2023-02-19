21e4d335730d6d5905b60146e1c9a8d7d956a21e
mixx-code <rfebriansyah385@gmail.com>
2023-02-19 20:06:21 +0700

    melakukan koneksi ke mongo membuat blog modul mengirim data ke mongo image blm bisa

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const multer = require("multer");
 
 const app = express();
+const authRoutes = require("./src/routes/auth");
+const blogRoutes = require("./src/routes/blog");
 
-app.use(() => {
-  console.log("hello server");
+// const fileStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "/public/images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, new Date().toString() + "-" + file.originalname);
+//   },
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   if (
+//     file.mimetype === "image/png" ||
+//     file.mimetype === "image/jpg" ||
+//     file.mimytype === "image/jpeg"
+//   ) {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
+
+app.use(bodyParser.json()); //type JSON
+// app.use(
+//   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+// );
+//eror cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.listen(4000);
+//GET '/users' ==> [{nama: Rizki}]
+app.use("/v1/blog", blogRoutes);
+app.use("/v1/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://rizki:kJl7aR5ADDvW2IFs@cluster0.hiuv4fm.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(4000, () => console.log("connection success"));
+  })
+  .catch((err) => console.log(err));
---
+++ src/controllers/blog.js
@@ -0,0 +1,48 @@
+const { validationResult } = require("express-validator");
+const BlogPost = require("../models/blog");
+
+exports.createBlogPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const err = new Error("Input value tidak sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
+  // if (!req.file) {
+  //   const err = new Error("Image Harus di Upload");
+  //   err.errorStatus = 422;
+  //   err.data = errors.array();
+  //   throw err;
+  // }
+
+  const title = req.body.title;
+  // const image = req.file.path;
+  const body = req.body.body;
+
+  const Posting = new BlogPost({
+    title,
+    // image: image,
+    body,
+    author: {
+      uid: 1,
+      name: "rizki",
+    },
+  });
+
+  Posting.save()
+    // .then((result) => {
+    //   res.status(201).json({
+    //     message: "Create Blog Post Success",
+    //     data: result,
+    //   });
+    // })
+    .then((result) => {
+      res.status(201).json({
+        message: "Create Blog Post Success",
+        data: result,
+      });
+    })
+    .catch((err) => console.log("err : ", err));
+};

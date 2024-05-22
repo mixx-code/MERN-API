983129f4dc3f9a623f93977a93959e7358219089
mixx-code <rfebriansyah385@gmail.com>
2024-05-22 13:08:40 +0700

    index

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
